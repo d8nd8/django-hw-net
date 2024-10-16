e4314da544e8f7ce7270c86481064de072cf2c70
d8nd8 <jumppmuj@yandex.ru>
2024-10-16 18:15:28 +0300

    HW2 added

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,23 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
-    },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
-    },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
-}
+        'omlet': {
+            'яйца, шт': 2,
+            'молоко, л': 0.1,
+            'соль, ч.л.': 0.5,
+        },
+        'pasta': {
+            'макароны, г': 0.3,
+            'сыр, г': 0.05,
+        },
+        'buter': {
+            'хлеб, ломтик': 1,
+            'колбаса, ломтик': 1,
+            'сыр, ломтик': 1,
+            'помидор, ломтик': 1,
+        },
+    }
+
+def recipes(request, recipe_name):
+    all_recipes = DATA
+
+    recipe = DATA.get(recipe_name, None)
+
+    return render(request, 'index.html', {'recipe': recipe})
+
+
+def get_servings(request, recipe_name):
+
+    recipe = DATA.get(recipe_name)
+
+    serving = int(request.GET.get('servings', 1))
+
+    if recipe:
+        new_recipe = {name: amount * serving for name, amount in recipe.items()}
+    else:
+        new_recipe = None
+
+    return render(request, 'index.html', {'recipe': new_recipe})
 
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.contrib import admin
 from django.urls import path
 
+from calculator.views import recipes, get_servings
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('admin/', admin.site.urls),
+    path('recipes/<str:recipe_name>/', recipes, name='recipes'),
+    path('<str:recipe_name>/', get_servings, name='get_servings'),
 ]
